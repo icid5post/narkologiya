b26b7f9803b274f7db1b80c559ed8dedb6b6360d
khasanovRustam <khasanovRustam@gmail.com>
2020-08-04 11:08:59 +0500

    assets has been added

--- src/js/main/app.js
+++ src/js/main/app.js
@@ -165,7 +165,16 @@ $(document).ready(function () {
 
 
 
-//Progress chart
-$(document).ready(function () {
-
+//Scroll top
+$(function(){
+    $(window).scroll(function(){
+        if($(document).scrollTop()>$(window).height()){
+            $('.scrolltotop').show();
+        }else{
+            $('.scrolltotop').hide();
+        }
+    });
+    $('.scrolltotop').click(function(){
+        $('html,body').animate({scrollTop: 0}, 1000);
+    });
 });
